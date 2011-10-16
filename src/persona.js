45a8a96b2c85dd1007850ba7f766146054f6842d
Federico "Lox" Lucignano <federico.lox@gmail.com>
2011-10-16 12:19:13 +0200

    Changed Traits syntax + code optimization + JSLint compliance

--- src/persona.js
+++ src/persona.js
@@ -8,25 +8,37 @@
 
 function Persona(){}
 function Trait(definition){
-  this._definition = definition;
-  this.getFeatures = function(){return this._definition.features;};
-  this.getBehaviours = function(){return this._definition.behaviours;};
+	this.characterize = definition.characterize;
+	this.behave = definition.behave;
 }
 
 Persona.describe = function(){
-  var persona = function(){
-    var features;
-    
-    for(var x = 0, t = this.traits, y = t.length; x < y; x++)
-      if(features = t[x].getFeatures()) features.apply(this, arguments);
-  },
-  behaviours;
-  
-  persona.prototype = new Persona();
-  persona.prototype.traits = arguments;
-  
-  for(var x = 0, y = arguments.length; x < y; x++)
-    if(behaviours = arguments[x].getBehaviours()) behaviours.apply(persona.prototype);
-  
-  return persona;
+	var persona = function(info){
+		var characterize,
+		x = 0,
+		t = this.traits,
+		y = t.length;
+		
+		for(; x < y; x++){
+			characterize = t[x].characterize;
+			
+			if(characterize)
+				characterize(this, info);
+		}
+	},
+	behave,
+	x = 0,
+	y = arguments.length;
+	
+	persona.prototype = new Persona();
+	persona.prototype.traits = arguments;
+	
+	for(; x < y; x++){
+		behave = arguments[x].behave;
+		
+		if(behave)
+			behave(persona.prototype);
+	}
+	
+	return persona;
 };
